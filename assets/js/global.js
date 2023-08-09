9cc6b3d4b81a6ee767c04a21d1dd89af241cc694
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-08-09 23:17:04 +0530

    fixed search-text-still-on-close bug

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,8 +2,8 @@
 
 // Add event on multiple elements
 
-const addEventOnElements = function(elements,eventType,callback){
-    for (const elem of elements) elem.addEventListener(eventType,callback);
+const addEventOnElements = function (elements, eventType, callback) {
+    for (const elem of elements) elem.addEventListener(eventType, callback);
 }
 
 // Toggle search in mobile devices || small screens
@@ -11,8 +11,20 @@ const addEventOnElements = function(elements,eventType,callback){
 const searchBox = document.querySelector("[search-box]");
 const searchTogglers = document.querySelectorAll("[search-toggler]");
 
+
 addEventOnElements(searchTogglers, "click", function () {
-searchBox.classList.toggle("active");
+    searchBox.classList.toggle("active");
+});
+
+// removes the search query after clicking the close search icon
+addEventOnElements(searchTogglers, "click", function () {
+    document.getElementById('textfield1').value = "";
+});
+addEventOnElements(searchTogglers, "click", function () {
+    document.getElementById('textfield2').value = "";
+});
+addEventOnElements(searchTogglers, "click", function () {
+    document.getElementById('textfield3').value = "";
 });
 
 
