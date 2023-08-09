1bbf053bb76109ad67ba8367bbe1d19ea04c2122
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-08-09 21:47:32 +0530

    added mobile-responsiveness at viewpoints

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -37,6 +37,8 @@ export function sidebar() {
 
             <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=hi", "Hindi")'>Hindi</a>
 
+            <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=bn", "Bengali")'>Bengali</a>
+
             <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=ja", "Japanese")'>Japanese</a>
 
         </div>
